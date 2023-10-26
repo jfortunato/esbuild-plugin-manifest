0d796c94c6c86a8dee4e1a8d3e868ba71b66fc93
Justin Fortunato <jfortunato25@gmail.com>
2023-10-26 11:01:35 -0400

    ci: Add test coverage to run-tests workflow

--- jest.config.js
+++ jest.config.js
@@ -34,12 +34,10 @@ module.exports = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    "text",
+    "cobertura",
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
